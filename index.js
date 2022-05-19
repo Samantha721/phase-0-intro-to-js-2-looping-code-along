7ee7826bd340913c09ed7123448b26531787e574
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-05-18 20:52:05 -0700

    commit tree 479799d4f380a948da3bbcda6edfa466cb5217f7

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(arrayNames, event) {
+  let messages = [];
+  for (let i = 0; i < arrayNames.length; i++) {
+    messages.push(
+      `Thank you, ${arrayNames[i]}, for the wonderful ${event} gift!`
+    );
+  }
+  return messages;
+}
+function countDown() {
+  for (let down = 0; down < 11; down++) {
+    console.log(down);
+  }
+}
+
+// function writeCards(arrayNames) {
+//   for (let i = 0; i < arrayNames.length; i++);
+//   {
+//     `Thank you, ${arrayNames}, for the wonderful surprise gift!`;
+//   }
+//   return arrayNames;
+// }
